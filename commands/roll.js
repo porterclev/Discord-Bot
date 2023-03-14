a2d5c1c5f62a6a23d64e0fc6204371e39e7be331
porterclev <porterclev@gmail.com>
2023-03-13 22:07:09 -0700

    roll is now sent to database

--- commands/roll.js
+++ commands/roll.js
@@ -1,37 +1,48 @@
 const { EmbedBuilder } = require("discord.js");
-
-let diceQuantity = 3;
-let diceMax = 6;
-const diceArray = [];
-let rolled = "";
-
-function multiDice() {
-    while (diceArray.length < diceQuantity) {
-        const random = Math.floor(Math.random() * diceMax) + 1;
-        diceArray.push(random);
-    }
-    for (let i = 0; i < diceArray.length; i++) 
-    {
-        rolled += ":game_die:" + diceArray[i] + " ‎ ‎ ‎ ‎";
-    }
-    let total = 0;
-    for (let i = 0; i < diceArray.length; i += 1)
-    {
-        total += diceArray[i];
-    }
-    return rolled + "\n" + '**TOTAL: **' + total;
-}
+const con = require("../components/connection");
 
 module.exports = {
   callback: (message, args) => {
-    var target = message.mentions.users.first(); //discord user
+    // Creates dice embed and rolls dice
+    let diceQuantity = 3;
+    let diceMax = 6;
+    const diceArray = [];
+    let rolled = "";
+    function multiDice() {
+      while (diceArray.length < diceQuantity) {
+        const random = Math.floor(Math.random() * diceMax) + 1;
+        diceArray.push(random);
+      }
+
+      for (let i = 0; i < diceArray.length; i++) {
+        rolled += ":game_die:" + diceArray[i] + " ‎ ‎ ‎ ‎";
+      }
+
+      let total = 0;
+      for (let i = 0; i < diceArray.length; i += 1) {
+        total += diceArray[i];
+      }
+      return total;
+    }
+
+    let rollNum = multiDice();
     let embed = new EmbedBuilder()
-      
+
       .setColor(15548997)
       .setTitle(message.author.tag) //discord user's name
-      .setDescription(multiDice())
-    message.channel.send({ embeds: [embed] }); //sends embed to text channel message/command was sent in
-  },
+      .setDescription(rolled + "\n" + "**TOTAL: **" + rollNum);
 
-  
+    // Update the database with the new roll value
+    con.query(
+      "UPDATE discord_unity_data SET ? WHERE ID=?",
+      [{ Roll_Value: rollNum }, message.author.id],
+      (err, res) => {
+        if (err) {
+          console.log(err);
+        } else {
+          return message.channel.send({ embeds: [embed] }); //sends embed to text channel message/command was sent in
+        }
+      }
+    );
+  },
 };
