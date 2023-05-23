b4c817ef61868d28a92f10794e7a2c5506f3cbd2
porterclev <porterclev@gmail.com>
2023-05-22 17:56:21 -0700

    simple jude counter

--- commands/jude.js
+++ commands/jude.js
@@ -2,27 +2,16 @@ const { EmbedBuilder } = require("discord.js");
 
 module.exports = {
   callback: (message, args) => {
+    var increase = 0;
+    if (message.content.length > 5) {
+      increase = parseInt(message.content.slice(6, 7));
+    }
     var currentdate = new Date();
-
+    currentdate.setDate(currentdate.getDate() + increase);
     var judeDate = new Date();
-    judeDate.setFullYear(2023, 4, 5);
+    judeDate.setFullYear(2023, 4, 4);
     judeDate.setHours(14, 26, 30, 0);
     var newDateString = currentdate.getDate() - judeDate.getDate() + " days ";
-    /*+ currentdate.getMonth() -
-      judeDate.getMonth() +
-      " months " +
-      currentdate.getFullYear() -
-      judeDate.getFullYear() +
-      " years " +
-      currentdate.getHours() -
-      judeDate.getHours() +
-      " hours " +
-      currentdate.getMinutes() -
-      judeDate.getMinutes() +
-      " minutes "; */
-    /*      + currentdate.getSeconds() -
-      judeDate.getSeconds() +
-      " seconds   "; */
     let embed = new EmbedBuilder()
       .setDescription(newDateString + "of no Jude") //date and time
       .setImage("https://media.giphy.com/media/dQCLPqlnrRkpW/giphy.gif");
