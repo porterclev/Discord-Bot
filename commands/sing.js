d619be63a55d1df990cc3de5d1f9c7773851db75
porterclev <porterclev@gmail.com>
2023-06-09 17:15:30 -0700

    idk

--- commands/sing.js
+++ commands/sing.js
@@ -1,10 +1,10 @@
-const ytdl = require("ytdl-core");
+const ytdl = require("ytdl-core-discord");
 const fs = require("fs");
 const voice = require("@discordjs/voice");
-const { join } = require("path");
+const cache = "./components/Cache/YTmp3/";
 
 module.exports = {
-  callback: (message, args) => {
+  callback: async (message, args) => {
     let url = message.content.slice(6);
     if (url.slice(12, 23) !== "youtube.com") {
       message.channel.send("Please enter a youtube link");
@@ -14,23 +14,51 @@ module.exports = {
       throw "Invalid YouTube link";
     }
 
-    const stream = ytdl(url, { filter: "audioonly" });
+    // const stream = ytdl(url, { filter: "audioonly", format: "mp3" });
+    // const stream = ytdl(url, { filter: "audioonly" });
+    /* stream
+      .pipe(fs.createWriteStream(cache + message.author.username + ".mp3"))
+      .on("finish", () => {
+        message.channel.send({
+          files: [cache + message.author.username + ".mp3"],
+        });
+      }); */
 
-    const audioplayer = voice.createAudioPlayer();
-    voice
-      .joinVoiceChannel({
-        channelId: message.member.voice.channel.id,
-        guildId: message.guild.id,
-        adapterCreator: message.guild.voiceAdapterCreator,
-      })
-      .subscribe(audioplayer);
-
-    const resource = voice.createAudioResource(stream);
-
-    audioplayer.play(resource);
-    audioplayer.on(voice.AudioPlayerStatus.Idle, () => {
-      audioplayer.stop();
+    const connection = voice.joinVoiceChannel({
+      channelId: message.member.voice.channel.id,
+      guildId: message.guild.id,
+      adapterCreator: message.guild.voiceAdapterCreator,
     });
+    // const path = cache + message.author.username + ".mp3";
+    const stream = await ytdl(url, {
+      filter: "audioonly",
+      quality: "highestaudio",
+      highWaterMark: 1 << 25,
+    });
+    // stream.pipe(fs.createWriteStream(path)).on("finish", () => {});
+    const audioplayer = voice.createAudioPlayer();
+    const resource = voice.createAudioResource(stream, {
+      inputType: voice.StreamType.Opus,
+    });
+    audioplayer.play(resource);
+    connection.subscribe(audioplayer);
+
+    audioplayer.on(voice.AudioPlayerStatus.Buffering, () => {
+      console.log("Buffering");
+    });
+
+    audioplayer.on(voice.AudioPlayerStatus.Idle, () => {
+      console.log("Idle");
+    });
+
+    /* const resource = voice.createAudioResource(
+      "../components/Cache/YTmp3/Gangus.mp3"
+    ); */
+
+    /* audioplayer.on(voice.AudioPlayerStatus.Idle, () => {
+      //   fs.rmdir(cache + message.author.username + ".mp3", (err) => {});
+      audioplayer.stop();
+    }); */
     /* 
     if (message.content === ".pause") {
       audioplayer.pause();
