28c4fdf1c2b433bdac8ab64cb3741d2aae7cea99
porterclev <porterclev@gmail.com>
2024-02-16 14:25:48 -0800

    added comments

--- components/get-files.js
+++ components/get-files.js
@@ -1,26 +1,26 @@
-const fs = require('fs');
-
+const fs = require("fs");
+/**
+ * description: gets all files in a directory
+ * @param {string} dir - directory to get files from
+ * @param {string} suffix - file type to get
+ * @returns {array} - array of file paths (strings)
+ 
+ */
 const getFiles = (dir, suffix) => {
-    const files = fs.readdirSync(dir, {
-        withFileTypes: true,
-    })
+  const files = fs.readdirSync(dir, {
+    withFileTypes: true,
+  });
 
-    let commandFiles = [];
+  let commandFiles = [];
 
-    for(const file of files){
-        if(file.isDirectory()){
-            commandFiles = [
-                commandFiles,
-                getFiles(`${dir}/${file.name}`, suffix)
-            ]
-        }else if(file.name.endsWith(suffix)){
-            commandFiles.push(`${dir}/${file.name}`)
-        }
-
-
-
+  for (const file of files) {
+    if (file.isDirectory()) {
+      commandFiles = [commandFiles, getFiles(`${dir}/${file.name}`, suffix)];
+    } else if (file.name.endsWith(suffix)) {
+      commandFiles.push(`${dir}/${file.name}`);
     }
+  }
 
-    return commandFiles;
-}
+  return commandFiles;
+};
 module.exports = getFiles;
