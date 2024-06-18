0cc47be7c49e55a063bc210455580b3d37584250
porterclev <porterclev@gmail.com>
2024-06-18 16:06:20 -0700

    fixed instance issue

--- commands/scoreboard.js
+++ commands/scoreboard.js
@@ -1,17 +1,22 @@
-const { EmbedBuilder } = require("discord.js");
+const { ActionRowBuilder, ButtonBuilder, EmbedBuilder, ComponentType } = require("discord.js");
 const { embedButtonInterface } = require("../components/embedInterface");
 const { createList } = require("../components/createButtons");
-const button = createList(
-  {
-    nextBoard: "Next Board"
-  },
-  "Primary"
-);
 let boards = ["Sekiro", "Elden Ring"];
 let boardIcons = ["https://i.imgur.com/Br4NfBm.jpeg", "https://i.imgur.com/bxy3Fpq.jpeg"]
 
 module.exports = {
   callback: (message, args, db) => {
+    let nextBoardId = Math.floor(Math.random() * 100000);
+    let button = new ActionRowBuilder();
+
+    button.addComponents(
+      new ButtonBuilder()
+        .setCustomId(`${nextBoardId}`)
+        .setLabel(`Next Board`)
+        .setStyle(
+          "Primary"
+        )
+    );
     if (args[0] === "view") {
       var board = 0;
       let embed = new EmbedBuilder()
@@ -20,10 +25,10 @@ module.exports = {
           { name: 'Click Next Board', value: 'To view the next board', inline: false },
         )
       embedButtonInterface(message, embed, button);
-      const collector = message.channel.createMessageComponentCollector({ time: 60_000 });
+      const collector = message.channel.createMessageComponentCollector({ componentType: ComponentType.Button, time: 60_000 });
       collector.on("collect", async (i) => {
         // moving right
-        if (i.customId === "nextBoard") {
+        if (i.customId === nextBoardId.toString()) {
           if (board >= boards.length - 1) {
             board = 0;
           } else {
@@ -48,6 +53,9 @@ module.exports = {
               await i.update({ embeds: [message], components: [button] });
             });
         }
+      });
+      collector.on("end", async (collected) => {
+        console.log("Scoreboard Timeout")
       });
     }
   },
